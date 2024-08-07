18ad2c0ae45630abfa8e6ec02c4ade92d3ada9ed
oldmcdonnell <oldmcdonnell@gmail.com>
2024-08-07 17:24:22 -0400

    code sorting code from newest to oldest

--- index.js
+++ index.js
@@ -10,32 +10,32 @@ async function sortHackerNewsArticles() {
   await page.goto("https://news.ycombinator.com/newest");
 
   // Select the titles and their timestamps
-  const articles = await page.$$eval(".athing", (articles) => {
+  const articles = await page.$$eval(".itemlist .athing", (articles) => {
     return articles.slice(0, 100).map((article) => {
       const title = article.querySelector(".storylink").innerText;
-      const timestamp = article.nextElementSibling.querySelector(".age a").title;
+      const ageElement = article.nextElementSibling.querySelector(".age");
+      const timestamp = ageElement ? ageElement.title : null;
       return { title, timestamp };
     });
   });
 
   // Parse the date of the article
   articles.forEach((article) => {
-    const date = new Date(article.timestamp);
-    article.date = date;
+    if (article.timestamp) {
+      const date = new Date(article.timestamp);
+      article.date = date;
+    }
   });
 
   // Validate the sorting order from newest to oldest
   for (let i = 1; i < articles.length; i++) {
-    if (articles[i].date > articles[i - 1].date) {
+    if (articles[i].date && articles[i - 1].date && articles[i].date > articles[i - 1].date) {
       console.log('Articles are not sorted from newest to oldest.');
-      // await new Promise(resolve => setTimeout(resolve, 5000));
-      // await browser.close();
       return;
     }
   }
 
   console.log('Articles are correctly sorted from newest to oldest.');
-  await browser.close();
 }
 
 (async () => {
